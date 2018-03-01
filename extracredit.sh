47f6266644c6aed54f695d66964224ba964864e8
James  Green <jxg4323@cs.rit.edu>
2018-03-01 18:38:35 -0500

    Experiment one for bash done

---
+++ extracredit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Experiment 1: Display the help message to show command line options
+#Experiment 2: Testing to show that the generations will be incredibly low when the propability a tree will catch is 100
+./wildfire help | cat experiment1.txt
+
